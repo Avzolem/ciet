21c2141c5c655bbe62e13132dc9fc107031388da
Bart Lopez <dalopez@uach.mx>
2023-08-21 15:00:51 -0600

    services component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Products from "../components/Products";
+import Services from "../components/Services";
 
 export default function Home() {
-  return (
-    <div className="bg-white">
-      <Hero />
-      <Products />
-    </div>
-  );
+	return (
+		<div className="bg-white">
+			<Hero />
+			<Products />
+			<Services />
+		</div>
+	);
 }
