a6c69e489ce4412f66a888a9c2d94a0361d8a09b
Bart Solo <yosoybartsolo@gmail.com>
2023-08-22 10:55:03 -0600

    Footer on index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Hero from "../components/Hero";
 import Products from "../components/Products";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 export default function Home() {
-	return (
-		<div className="bg-white">
-			<Hero />
-			<Products />
-			<Services />
-		</div>
-	);
+  return (
+    <div className="bg-white">
+      <Hero />
+      <Products />
+      <Services />
+      <Footer />
+    </div>
+  );
 }
