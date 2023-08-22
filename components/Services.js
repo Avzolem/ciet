efdb7022af882709fdfa115b875514da23f3ffb1
Bart Lopez <dalopez@uach.mx>
2023-08-22 10:02:08 -0600

    New services component

--- components/Services.js
+++ components/Services.js
@@ -1,87 +1,121 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-const services = [
-	{
-		id: 1,
-		name: "Hybrida",
-		href: "https://hybrida.uach.mx",
-		imageSrc: "/images/products/imagen-hybrida.png",
-		logoSrc: "/images/products/logo-hybrida.png",
-		imageAlt: "Logo Campus Seguro App.",
-	},
-	{
-		id: 2,
-		name: "Eduhackers",
-		href: "#",
-		imageSrc: "/images/products/imagen-eduhackers.png",
-		logoSrc: "/images/products/logo-eduhackers.png",
-		imageAlt:
-			"Olive drab green insulated bottle with flared screw lid and flat top.",
-	},
-	{
-		id: 3,
-		name: "Train the teachers",
-		href: "https://teachers-xi.vercel.app/",
-		imageSrc: "/images/products/imagen-train.png",
-		logoSrc: "/images/products/logo-train.png",
-		imageAlt:
-			"Person using a pen to cross a task off a productivity paper card.",
-	},
-	{
-		id: 4,
-		name: "Hybrida",
-		href: "https://hybrida.uach.mx",
-		imageSrc: "/images/products/imagen-hybrida.png",
-		logoSrc: "/images/products/logo-hybrida.png",
-		imageAlt: "Hybrida.",
-	},
+import {
+	AcademicCapIcon,
+	BanknotesIcon,
+	CheckBadgeIcon,
+	ClockIcon,
+	ReceiptRefundIcon,
+	UsersIcon,
+} from "@heroicons/react/24/outline";
 
-	// More products...
+const actions = [
+	{
+		title: "Design thinking",
+		href: "#",
+		icon: ClockIcon,
+		iconForeground: "text-teal-700",
+		iconBackground: "bg-teal-50",
+	},
+	{
+		title: "Metodología Lean Startup",
+		href: "#",
+		icon: CheckBadgeIcon,
+		iconForeground: "text-purple-700",
+		iconBackground: "bg-purple-50",
+	},
+	{
+		title: "Fábrica de ideas",
+		href: "#",
+		icon: UsersIcon,
+		iconForeground: "text-sky-700",
+		iconBackground: "bg-sky-50",
+	},
+	{
+		title: "ABC del diseño gráfico",
+		href: "#",
+		icon: BanknotesIcon,
+		iconForeground: "text-yellow-700",
+		iconBackground: "bg-yellow-50",
+	},
+	{
+		title: "iCrea",
+		href: "#",
+		icon: ReceiptRefundIcon,
+		iconForeground: "text-rose-700",
+		iconBackground: "bg-rose-50",
+	},
+	{
+		title: "Training",
+		href: "#",
+		icon: AcademicCapIcon,
+		iconForeground: "text-indigo-700",
+		iconBackground: "bg-indigo-50",
+	},
 ];
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
 
 export default function Example() {
 	return (
-		<div className="bg-white">
-			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-				<h2 className="text-black text-5xl pb-10 text-center font-semibold">
-					Nuestros servicios
-				</h2>
-
-				<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-					{services.map((service) => (
-						<a key={service.id} href={service.href} className="group">
-							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7 relative">
-								<img
-									src={service.imageSrc}
-									alt={service.imageAlt}
-									className="h-full w-full object-cover object-center group-hover:opacity-75"
-								/>
-								<img
-									src={service.logoSrc}
-									alt={service.imageAlt}
-									className="absolute w-44 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 group-hover:opacity-75"
-								/>
-							</div>
-							<h3 className="mt-4 text-lg font-semibold text-gray-700">
-								{service.name}
+		<div>
+			<h2 className="text-black text-5xl pb-10 text-center font-semibold">
+				Nuestros servicios
+			</h2>
+			<div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+				{actions.map((action, actionIdx) => (
+					<div
+						key={action.title}
+						className={classNames(
+							actionIdx === 0
+								? "rounded-tl-lg rounded-tr-lg sm:rounded-tr-none"
+								: "",
+							actionIdx === 1 ? "sm:rounded-tr-lg" : "",
+							actionIdx === actions.length - 2 ? "sm:rounded-bl-lg" : "",
+							actionIdx === actions.length - 1
+								? "rounded-bl-lg rounded-br-lg sm:rounded-bl-none"
+								: "",
+							"group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+						)}>
+						<div>
+							<span
+								className={classNames(
+									action.iconBackground,
+									action.iconForeground,
+									"inline-flex rounded-lg p-3 ring-4 ring-white"
+								)}>
+								<action.icon className="h-6 w-6" aria-hidden="true" />
+							</span>
+						</div>
+						<div className="mt-8">
+							<h3 className="text-base font-semibold leading-6 text-gray-900">
+								<a href={action.href} className="focus:outline-none">
+									{/* Extend touch target to entire panel */}
+									<span className="absolute inset-0" aria-hidden="true" />
+									{action.title}
+								</a>
 							</h3>
-							<p className="mt-1 text-lg font-medium text-gray-900"></p>
-						</a>
-					))}
-				</div>
+							<p className="mt-2 text-sm text-gray-500">
+								Doloribus dolores nostrum quia qui natus officia quod et
+								dolorem. Sit repellendus qui ut at blanditiis et quo et
+								molestiae.
+							</p>
+						</div>
+						<span
+							className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+							aria-hidden="true">
+							<svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+								<path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+							</svg>
+						</span>
+					</div>
+				))}
 			</div>
 		</div>
 	);
 }
+
+//Design Thinking
+//Metodología Lean Startup
+//Fabrica de Ideas
+//ABC del Diseño Grafico
