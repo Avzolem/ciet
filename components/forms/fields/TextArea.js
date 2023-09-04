3c0c09184a0ad76e351c4288217983b3720dbe08
Bart Lopez <dalopez@uach.mx>
2023-09-04 12:16:39 -0600

    text area border gray

--- components/forms/fields/TextArea.js
+++ components/forms/fields/TextArea.js
@@ -1,36 +1,33 @@
 const TextArea = ({
-    label,
-    name,
-    type,
-    placeholder = "",
-    errorMessage = "",
-    register,
-    ...rest
+	label,
+	name,
+	type,
+	placeholder = "",
+	errorMessage = "",
+	register,
+	...rest
 }) => {
-    const className = { ...rest.className };
+	const className = { ...rest.className };
 
-    return (
-        <div className={className}>
-            <label
-                htmlFor={name}
-                className="block text-sm font-medium text-white"
-            >
-                {label}
-            </label>
-            <div className="mt-1">
-                <textarea
-                    rows={4}
-                    name={name}
-                    id={name}
-                    className="focus:ring-buttonbg focus:border-buttonbg block w-full rounded-sm border-gray-300 placeholder-gray-300 shadow-sm sm:text-sm"
-                    placeholder={placeholder}
-                    defaultValue={""}
-                    {...register}
-                />
-            </div>
-            <p className="mt-2 text-sm text-red-600">{errorMessage}</p>
-        </div>
-    );
+	return (
+		<div className={className}>
+			<label htmlFor={name} className="block text-sm font-medium text-white">
+				{label}
+			</label>
+			<div className="mt-1">
+				<textarea
+					rows={4}
+					name={name}
+					id={name}
+					className="border focus:ring-buttonbg focus:border-buttonbg block w-full rounded-sm border-gray-200 placeholder-gray-300 shadow-sm sm:text-sm"
+					placeholder={placeholder}
+					defaultValue={""}
+					{...register}
+				/>
+			</div>
+			<p className="mt-2 text-sm text-red-600">{errorMessage}</p>
+		</div>
+	);
 };
 
 export default TextArea;
